9b6f3eef8a4a45d8eff80eaecf443a2097e60ae8
orwyn <owryn.carvalho@gmail.com>
2024-02-05 11:33:29 +0530

    - view keymapping shortcut

--- lua/droid/mapping.lua
+++ lua/droid/mapping.lua
@@ -48,6 +48,7 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/droid/packer.lua<CR>");
+vim.keymap.set("n", "<leader>vkm", "<cmd>e ~/.config/nvim/lua/droid/mapping.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 --remap save file
