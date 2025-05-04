a32baab97afaf4f94455e4ce11d153c54827a1dc
orwyn <owryn.carvalho@gmail.com>
2025-05-04 21:44:21 +0530

    mofify key mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.del("n", "<leader>ss", vim.cmd.Ex)
+vim.keymap.del("n", "<leader>q", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.keymap.del("n", "<leader>wq", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>wq", ":wq<CR>")
